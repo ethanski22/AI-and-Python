f3bc5b857e83c152ea40e63082f2f2518025c2c7
Ethan Skowronski <ethanski22@icloud.com>
2025-02-05 20:31:14 -0500

    Join/Leave Server
    Sends a message whenever someone leaves or joins the server

--- AI Practice/Discord/Discord.py
+++ AI Practice/Discord/Discord.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import requests
 from dotenv import load_dotenv
 from discord.ext import commands
 from pathlib import Path
@@ -10,15 +11,36 @@ load_dotenv(dotenv_path=env_path)
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = commands.Bot(command_prefix = "/", intents = intents)
 
+# Events
 @client.event
 async def on_ready():
     print("Bot is ready.")
 
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server")
+\
+# Use to send a message to a specific channel
+# @client.event
+# async def on_member_join(member):
+#     channel = client.get_channel(1336498123102486619)
+#     await channel.send(f"{member} has joined the server")
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left the server")
+
+# Commands
 @client.command(name = "hello")
 async def hello(ctx):
     await ctx.send("Hello there")
+
+@client.command(name = "ping")
+async def ping(ctx):
+    await ctx.send("pong")
 
 client.run(os.environ.get("DISCORD_TOKEN"))
