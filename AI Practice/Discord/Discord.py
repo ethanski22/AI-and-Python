e79855acc8bce545eff8870f309aa656b10254e1
Ethan Skowronski <ethanski22@icloud.com>
2025-02-06 19:41:19 -0500

    Embeds
    Added the ability to call embeds

--- AI Practice/Discord/Discord.py
+++ AI Practice/Discord/Discord.py
@@ -43,4 +43,23 @@ async def hello(ctx):
 async def ping(ctx):
     await ctx.send("pong")
 
+# Embeds
+@client.command(name = "embed")
+async def embed(ctx):
+    embed = discord.Embed(
+        title = "Title",
+        description = "This is a description",
+        color = discord.Color.blue()
+    )
+
+    embed.set_footer(text = "This is a footer")
+    embed.set_image(url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
+    embed.set_thumbnail(url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
+    embed.set_author(name = "Author", icon_url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
+    embed.add_field(name = "Field Name", value = "Field Value", inline = False)
+    embed.add_field(name = "Field Name", value = "Field Value", inline = False)
+
+    await ctx.send(embed = embed)
+
+
 client.run(os.environ.get("DISCORD_TOKEN"))
