2923b08bc6d915748585813fc9de9ea3672de6c6
Ethan Skowronski <eskowronski@lcs.com>
2025-02-19 15:32:44 -0500

    Created Speech to Text

---
+++ AI Practice/Code/SpeechToText.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+audio_file = open("speech.mp3", "rb")
+
+transcript = client.audio.transcriptions.create(
+  model = "whisper-1",
+  file = audio_file,
+  language = "en",
+  audio_format = "mp3",
+  response_format = "text"
+)
