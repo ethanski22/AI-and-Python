9e2743d06ae2f55eca86843ce46566af9b7e6f28
Ethan Skowronski <eskowronski@lcs.com>
2025-02-19 15:23:05 -0500

    Fixed write to file

--- AI Practice/Code/TextToSpeech.py
+++ AI Practice/Code/TextToSpeech.py
@@ -1,12 +1,20 @@
 from openai import OpenAI
+from pathlib import Path
 
 client = OpenAI()
 
 response = client.audio.speech.create(
-    model="tts-1",
-    voice="echo",
+    model = "tts-1",
+    voice = "echo",
     response_format = "ogg",
-    input="Hello world! This is a streaming test.",
+    input = "Hello world! This is a streaming test.",
 )
 
-response.stream_to_file("output.ogg")
+# response.stream_to_file("output.ogg")
+
+audioPath = Path("AI Practice\AudioFiles\output.ogg")
+
+# Open file and write the response
+with open(audioPath, "wb") as file:
+    for chunk in response.iter_bytes():
+        file.write(chunk)
